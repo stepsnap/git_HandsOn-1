435e07d49f190e081044271be40387026e132fc1
stepsnap <101996269+stepsnap@users.noreply.github.com>
2022-04-13 13:23:53 +0200

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,11 +1,12 @@
+
 aCount = 0
 cCount = 0
 tCount = 0
 gCount = 0
 uCount = 0
 
-for char in Seq.upper():
-    if char == "A":
+for char in Seq.upper(): # To converts all lowercase characters in a string into uppercase characters and returns it.
+    if char == "A":nt
         aCount = aCount + 1
     elif char == "C":
         cCount = cCount + 1
@@ -16,8 +17,8 @@ for char in Seq.upper():
     elif char == "U":
         uCount = uCount + 1
 
-print ("Percentage of A:", (float(aCount) / len(Seq)) * 100)
-print ("Percentage of C:", (float(cCount) / len(Seq)) * 100)
-print ("Percentage of T:", (float(tCount) / len(Seq)) * 100)
-print ("Percentage of G:", (float(gCount) / len(Seq)) * 100)
-print ("Percentage of U:", (float(uCount) / len(Seq)) * 100)
+print ("Percentage of A:", (float(aCount) / len(Seq)) * 100)#Print the A Percentage
+print ("Percentage of C:", (float(cCount) / len(Seq)) * 100)#Print the C Percentage
+print ("Percentage of T:", (float(tCount) / len(Seq)) * 100)#Print the T Percentage
+print ("Percentage of G:", (float(gCount) / len(Seq)) * 100)#Print the G Percentage
+print ("Percentage of U:", (float(uCount) / len(Seq)) * 100)#Print the U Percentage
